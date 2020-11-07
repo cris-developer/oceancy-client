fbeb6c12821449fca8f6c5a7a7805ae9d6e9ce30
Cristina Matamoro <cristina.matamoro@gmail.com>
2020-11-07 18:06:48 +0100

    added styling to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 //import SearchBar from '../../components/searchbar/SearchBar.js';
 import Carousel from '../../components/carousel/Carousel.js';
 //import {Carousel} from 'react-bootstrap'
-//import { AnimateOnChange } from 'react-animation'
+
 
 import './Home.css'
 
@@ -31,19 +31,21 @@ export default class Home extends Component {
         <div className ='search-results-container'>
 
             {this.state.searchActivities  ? this.state.searchActivities.map((el, idx) => (
-              <div key={idx}>   
-                  <img src={el.photoUrl} alt="ActivityList" className="img"  style={{width:'10%' ,display:'flex',flexDirection:'row' }}/>
-                  <div className= 'contentText'>  {el.name}</div>
-                  <div>{el.description}</div> 
-    
+              <div key={idx}> 
+               <div className='HomeCardContainer'>
+                  <div className="HomeCardImage"> 
+                    <img src={el.photoUrl} alt="ActivityList" className="img"/>
+                  </div>
+                 
+                <div className="cardInfo" >
+                    <div className= 'contentText'>  {el.name}</div>
+                    <div>{el.description}</div> 
+                </div>
+            </div>
               </div>        
           )): ''}
         </div>
-        {/* <SearchBar/> */}
-        {/* <h1>welcome {username && props.user.username}</h1> */}
-        {/* <AnimateOnChange>
-          <h1 className= 'titleHeader' style ={{marginTop : '400px'}}>Enjoy your holidays</h1>
-        </AnimateOnChange> */}
+      
       </div>
     );
   }
